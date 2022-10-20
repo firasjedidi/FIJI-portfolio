7e318f2586cd4588fdf7b4c9c42286187aeadc4f
firasjedidi <firasjedidi.com>
2022-10-20 14:35:14 +0100

    fixing  about

--- pages/About.tsx
+++ pages/About.tsx
@@ -14,15 +14,15 @@ const wraper={
         <Grid item md={6} lg={6} sx={wraper} sm={6} xs={12}>
             <Image layout="responsive" width={40}  height={40}  objectFit="scale-down" alt='developer'   src="https://res.cloudinary.com/dod9yhmlt/image/upload/v1665852238/man-thinking-about-load-561228-unscreen_glybkn.gif"   blurDataURL='true' />
         </Grid>
-        <Grid item lg={6} md={6} sm={6} xs={12}  sx={{margin:{xs:"-11em 0",sm:"13em 0",md:"15em 01em",lg:"15em 3em" }}}>
+        <Grid item lg={6} md={6} sm={6} xs={12}  sx={{margin:{xs:"",sm:"13em 0",md:"15em 01em",lg:"15em 3em" }}}>
           <Box>
-          <Typography m={2} >
+          <Typography m={1.5} >
             ABOUT
           </Typography>
-          <Typography variant='h3' m={2}>
+          <Typography variant='h3' m={1.5}>
             who I Am
           </Typography>
-          <Typography  sx={{color:"#fff"}}  m={2}>
+          <Typography  sx={{color:"#fff"}}  m={1.5}>
           Hello once more I&apos;m Firas Jedidi, a Tunisian citizen, and I&apos;m not going to reveal my age for a few reasons. I studied computer science and dropped out of high school dropout before deciding to take a shortcut to become who I am today.
           </Typography>
           </Box>
